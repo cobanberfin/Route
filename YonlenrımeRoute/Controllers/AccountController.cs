0e95b92fec64f1092be5ee50b7a4a17f4262f0cb
berfin çoban <73108874+cobanberfin@users.noreply.github.com>
2022-02-03 10:51:35 +0300

    Add project files.

---
+++ YonlenrımeRoute/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YonlenrımeRoute.Models;
+
+namespace YonlenrımeRoute.Controllers
+{
+    public class AccountController : Controller
+    {
+        kullaniciEntities db = new kullaniciEntities();
+        // GET: Account
+        [Route("register")]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("register")]
+        public ActionResult Register(usertablo user)
+        {
+            db.usertabloes.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("Profile",new { username=user.firstname});
+        }
+        [Route("u/{username}")]//kullanıcıdan  gelen
+        public ActionResult Profile(string username)
+        {
+            var user = db.usertabloes.FirstOrDefault(x => x.firstname == username);
+            if (user == null)
+                return new HttpNotFoundResult();
+            return View(user);
+        }
+    }
+}
---
+++ YonlenrımeRoute/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YonlenrımeRoute.Controllers
+{[RoutePrefix("Main")]//ön ek olarak yazar
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("~/about")]//main ifadesi  gelm ez ~işareti onu  harıcı tutar
+        public ActionResult About()
+        {
+            return View();
+        }
+        [Route("contact-us")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        [Route("~/Demo/{number=1}/{id?}")]//ıd boş olabılır gırılmedende sayfay gırsın
+        public ActionResult Demo(int number,int? id)
+        {
+            return Content(string.Format("Number={0}<br/> Id={1}",number,id.HasValue?id.ToString():"null"));
+        }
+    }
+}
